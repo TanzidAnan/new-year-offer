7222a89fecc297f22319c2079fc1a73e94b5560b
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-17 00:41:47 +0600

    main

--- src/compontents/MainLayout/MainLayout.jsx
+++ src/compontents/MainLayout/MainLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navber from "../Navber/Navber";
 
 const MainLayout = () => {
     return (
         <div>
-            I am main layout
+            <Navber></Navber>
             <Outlet></Outlet>
         </div>
     );
