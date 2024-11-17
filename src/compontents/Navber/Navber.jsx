cbf6067401dd9767573d71c69b47ef0c5f7f419f
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-17 11:30:20 +0600

    about

--- src/compontents/Navber/Navber.jsx
+++ src/compontents/Navber/Navber.jsx
@@ -1,8 +1,18 @@
+import { NavLink } from 'react-router-dom';
 import './Navber.css'
 const Navber = () => {
     return (
         <div className='min-h-16 bg-red-300'>
-            this is Navber
+            <div>
+                <button>TEETH_MAN</button>
+            </div>
+            <div>
+                <NavLink to='/' className={({isActive}) =>isActive?'text-green-600':''}>Home</NavLink>
+                <NavLink to='/about' className={({isActive}) =>isActive?'text-green-600':''}>All Treatment</NavLink>
+                <NavLink to='/'>My Apperments</NavLink>
+                <NavLink>Profile</NavLink>
+            </div>
+            <div></div>
         </div>
     );
 };
