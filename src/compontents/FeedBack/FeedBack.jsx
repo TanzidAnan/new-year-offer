ffc144f9bc333d21d4c48128a6ee663fb6b3216c
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-29 21:44:18 +0600

    data

--- src/compontents/FeedBack/FeedBack.jsx
+++ src/compontents/FeedBack/FeedBack.jsx
@@ -3,33 +3,35 @@
 const FeedBack = ({ feedback }) => {
     console.log(feedback)
     return (
-        <div className="grid grid-cols-3 gap-3">
-            {
-                feedback.map((feed, idx) => <div key={idx} className="card bg-slate-200 w-96 shadow-xl">
-                    <div className="card-body">
-                        <div className="flex gap-4">
-                            <img src={feed.img} className="w-14 h-14 rounded-full" alt="" />
-                            <h2 className="card-title">{feed.name}</h2>
-                        </div>
-                        <p>{feed.review}</p>
-
-                        <div className="card-actions justify-end items-center">
-                            <div className="rating">
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input
-                                    type="radio"
-                                    name="rating-2"
-                                    className="mask mask-star-2 bg-orange-400"
-                                    defaultChecked />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+        <div>
+            <div className="grid grid-cols-3 gap-3">
+                {
+                    feedback.map((feed, idx) => <div key={idx} className="card bg-slate-200 w-96 shadow-xl">
+                        <div className="card-body">
+                            <div className="flex gap-4">
+                                <img src={feed.img} className="w-14 h-14 rounded-full" alt="" />
+                                <h2 className="card-title">{feed.name}</h2>
                             </div>
-                            <button className="btn btn-primary">Buy Now</button>
+                            <p>{feed.review}</p>
+
+                            <div className="card-actions justify-end items-center">
+                                <div className="rating">
+                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                    <input
+                                        type="radio"
+                                        name="rating-2"
+                                        className="mask mask-star-2 bg-orange-400"
+                                        defaultChecked />
+                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                </div>
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
                         </div>
-                    </div>
-                </div>)
-            }
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
