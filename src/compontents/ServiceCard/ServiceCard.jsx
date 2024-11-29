8c437b8a58ea9abd1f823e74cfd405133817cdc8
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-29 22:13:42 +0600

    button click

--- src/compontents/ServiceCard/ServiceCard.jsx
+++ src/compontents/ServiceCard/ServiceCard.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 
 const ServiceCard = ({ service }) => {
-    const {image,treatment_description,treatment_name,cost} =service
+    const {id,image,treatment_description,treatment_name,cost} =service
     console.log(service)
-    const hendleChecking =() =>{
-        console.log(6565656565)
+    const hendleChecking =(id) =>{
+        console.log(id)
     }
     return (
         <div>
@@ -22,7 +22,7 @@ const ServiceCard = ({ service }) => {
                     <p className="text-lg font-bold text-lime-400">Price: ${cost}</p>
                     <p title={treatment_description}>{treatment_description.slice(0,100)} </p>
                     <div className="card-actions justify-end">
-                        <div onClick={hendleChecking} className="badge badge-outline font-bold p-5 cursor-pointer bg-green-900 text-white">Chack Out More</div>
+                        <div onClick={() =>hendleChecking(id)} className="badge badge-outline font-bold p-5 cursor-pointer bg-green-900 text-white">Chack Out More</div>
                     </div>
                 </div>
             </div>
