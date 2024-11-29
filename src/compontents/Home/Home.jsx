9b68c75c5fdd45d6f97c649352770560817d2e30
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-29 22:05:16 +0600

    data

--- src/compontents/Home/Home.jsx
+++ src/compontents/Home/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
                     serviceData.slice(0,5).map((service,idx) => <ServiceCard key={idx} service={service}></ServiceCard>)
                 }
             </div>
-            <button className="mx-auto block text-red-200 btn btn-primary"><NavLink to='/allTreatment' >See More</NavLink></button>
+            <button className="mx-auto block text-red-200 btn bg-green-900 my-7"><NavLink to='/allTreatment' >See More</NavLink></button>
 
             <FeedBack feedback={feedback} ></FeedBack>
         </div>
