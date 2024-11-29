c3705e70da5cda152480f796fc7082414d0983fe
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-29 23:03:14 +0600

    prames

--- src/compontents/ProductDetles/ProductDetles.jsx
+++ src/compontents/ProductDetles/ProductDetles.jsx
@@ -1,5 +1,9 @@
+import { useLoaderData, useParams } from "react-router-dom";
 
 const ProductDetles = () => {
+    const allData =useLoaderData();
+    const {id}=useParams();
+    console.log(parseInt(id))
     return (
         <div>
             
