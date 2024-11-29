21c47a1f01aa699825e2b3cdf00f8bbe431b2b7d
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-29 23:21:03 +0600

    data

--- src/compontents/ProductDetles/ProductDetles.jsx
+++ src/compontents/ProductDetles/ProductDetles.jsx
@@ -2,8 +2,11 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 const ProductDetles = () => {
     const allData =useLoaderData();
+    console.log(allData)
     const {id}=useParams();
     console.log(parseInt(id))
+    const productId =allData.find(data => data.id ===parseInt(id));
+    console.log(productId)
     return (
         <div>
             
